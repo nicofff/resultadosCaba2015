c55ed70ff97176138222c2a60a428913f7ee07ce
Nicolas Far <nicofff@gmail.com>
2015-07-06 09:25:09 -0300

    Agregado checkeo de totales por categorias

--- totalElectores.py
+++ totalElectores.py
@@ -21,6 +21,34 @@ for mesa in os.listdir(os.getcwd()+"/data"):
 				print "Diferencia de votos entre categorias " + nice_mesa
 				error = True
 
+			### Total Jefe de Gobierno
+			totalJefe = 0
+			for listaJefe in datosMesa["datos"]["jefes"]["listas"]:
+				totalJefe += listaJefe["votos"]
+
+			if totalJefe != votantesJefeGob:
+				print "Diferencia en total Jefe de Gobierno. Mesa "+ nice_mesa
+				error=True
+
+
+			### Total Legisladores
+			totalLegislador = 0
+			for listaLegislador in datosMesa["datos"]["legisladores"]["listas"]:
+				totalLegislador += listaLegislador["votos"]
+
+			if totalLegislador != votantesLegislador:
+				print "Diferencia en total legisladores. Mesa "+ nice_mesa
+				error=True
+
+			### Total Comuneros
+			totalComunero = 0
+			for listaComuneros in datosMesa["datos"]["comuneros"]["listas"]:
+				totalComunero += listaComuneros["votos"]
+
+			if totalComunero != votantesComunero:
+				print "Diferencia en total comuneros. Mesa "+ nice_mesa
+				error=True
+
 			#if (not error):
 			#	print "Mesa " + nice_mesa + " OK"
 
