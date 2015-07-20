076aab63766de35570b8133bce0c0bb0913cf783
Nicolas Far <nicofff@gmail.com>
2015-07-19 23:59:10 -0300

    Ploteo porcentages mesas

--- plotMesas.py
+++ plotMesas.py
@@ -2,27 +2,36 @@ import os
 from mesa import Mesa
 
 mesas = Mesa.leer_todas(os.getcwd()+"/data")
-
+print "mesa registrados votaron pro eco blanco impugnado nulo recurrido tecnico"
 for mesa in mesas:
+
 	pro=""
 	eco=""
-	fpv=""
-	ayl=""
-	fit=""
+	blanco=""
+	imp=""
+	nul=""
+	rec=""
+	tec=""
 	error = False
 	votosListas = {}
 	if mesa.get_cargada():
+		votantes =mesa.get_resumen("VotantesMesa")
+		votaron = mesa.get_resumen("VotantesJef")
 		for listaJefe in mesa.get_listas("jefes"):
 			if listaJefe["id_lista"] == "18":
-				pro = str(listaJefe["pct_valido"])
+				pro = str(listaJefe["votos"])
 			elif listaJefe["id_lista"] == "16":
-				eco = str(listaJefe["pct_valido"])
-			elif listaJefe["id_lista"] == "23":
-				fpv = str(listaJefe["pct_valido"])
-			elif listaJefe["id_lista"] == "81":
-				ayl = str(listaJefe["pct_valido"])
-			elif listaJefe["id_lista"] == "17":
-				fit = str(listaJefe["pct_valido"])
+				eco = str(listaJefe["votos"])
+			elif listaJefe["id_lista"] == "BLC":
+				blanco = str(listaJefe["votos"])
+			elif listaJefe["id_lista"] == "IMP":
+				imp = str(listaJefe["votos"])
+			elif listaJefe["id_lista"] == "NUL":
+				nul = str(listaJefe["votos"])
+			elif listaJefe["id_lista"] == "REC":
+				rec = str(listaJefe["votos"])
+			elif listaJefe["id_lista"] == "TEC":
+				tec = str(listaJefe["votos"])
 
-		print "%s %s %s %s %s %s" % (mesa.get_numero(),pro,eco,fpv,ayl,fit)
+		print "%s %s %s %s %s %s %s %s %s %s" % (mesa.get_numero(),votantes,votaron,pro,eco,blanco,imp,nul,rec,tec)
 
